064a96f8aa25f43f3a710b69037e56b932f7404f
Susannah <susannah@pobox.com>
2022-09-19 07:36:26 -0700

    build file for climatebc input

--- scripts/prepare_location_files.R
+++ scripts/prepare_location_files.R
@@ -1,3 +1,5 @@
+# Add elevation to all locations and format a location file for ClimateBC
+
 library(dplyr)
 library(rgbif)
 
@@ -9,19 +11,27 @@ library(rgbif)
 site_coord <- read.csv("locations/site_coordinates.csv", header = TRUE, stringsAsFactors = FALSE)
 
 siteelevs <- rgbif::elevation(latitude= site_coord$lat, longitude = site_coord$lon, username="susannah2")
-colnames(siteelevs) <- c("lat", "lon", "elev")
-site_coord_elev <- dplyr::full_join(seed_orchard_sites, siteelevs)
+colnames(siteelevs) <- c("lat", "lon", "el")
+site_coord_elev <- dplyr::full_join(site_coord, siteelevs)
 
 write.csv(site_coord_elev, "locations/site_coord_elev.csv", row.names = FALSE)
 
 ## for pcic gridpoints
 
-grid_coord <- read.csv("locations/grid_coordinates.csv", header = TRUE, stringsAsFactors = FALSE)
+grid_coord <- read.csv("locations/grid_coordinates.csv", header = TRUE, stringsAsFactors = FALSE) %>%
+    rename(lat = Grid_lat, lon = Grid_lon)
 
-gridelevs <- rgbif::elevation(latitude = grid_coord$Grid_lat, longitude=grid_coord$Grid_lon, username="susannah2")
-colnames(gridelevs) <- c("Grid_lat", "Grid_lon", "Grid_elev")
-grid_coord_elev <- dplyr::full_join(gridelevs, site_coord_elev)
+gridelevs <- rgbif::elevation(latitude = grid_coord$lat, longitude=grid_coord$lon, username="susannah2")
+colnames(gridelevs) <- c("lat", "lon", "el")
+grid_coord_elev <- dplyr::full_join(gridelevs, grid_coord) %>%
+    select(-contains("Site_"))
 
-#write.csv(grid_coord_elev, "locations/grid_coord_elev.csv", row.names = FALSE)
+# Combine site and grid square location info ####
 
-# Combine site and grid square location info for ClimateBC
+climatebc_locs <- site_coord_elev %>%
+    select(Site, lat, lon, el) %>%
+    mutate(id = "site") %>%
+    full_join(mutate(grid_coord_elev, id = "grid")) %>%
+    select(Site, id, lat, lon, el)
+
+write.csv(climatebc_locs, "locations/climatebc_locs.csv", row.names = FALSE, eol = "\r\n")
