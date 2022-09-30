7d4d93fe577181ce072cda26ed3c9e6422929ec6
Susannah <susannah@pobox.com>
2022-09-30 10:28:05 -0700

    add locations for parent trees

--- scripts/prepare_location_files.R
+++ scripts/prepare_location_files.R
@@ -1,11 +1,16 @@
-# Add elevation to all locations and format a location file for ClimateBC
+# Add elevation to locations and format location files for ClimateBC.
+
+# Site_coordinates are seed orchard locations plus additional sites used for model projections
+# Grid coordinates are the coordinates of the PNWNAmet grid locations closest to site coordinates
+# Parents are the locations and elevations of the parent trees of seed orchard trees.
 
 library(dplyr)
 library(rgbif)
 
+# site & grid ######################
 # - get elevation data for sites and grid squares containing those sites, then combine and write out.
 
-# get and write elevation data####
+# get and write elevation data
 ## for sites
 
 site_coord <- read.csv("locations/site_coordinates.csv", header = TRUE, stringsAsFactors = FALSE)
@@ -26,7 +31,7 @@ colnames(gridelevs) <- c("lat", "lon", "el")
 grid_coord_elev <- dplyr::full_join(gridelevs, grid_coord) %>%
     select(-contains("Site_"))
 
-# Combine site and grid square location info ####
+# Combine site and grid square location info
 
 climatebc_locs <- site_coord_elev %>%
     select(Site, lat, lon, el) %>%
@@ -35,3 +40,22 @@ climatebc_locs <- site_coord_elev %>%
     select(Site, id, lat, lon, el)
 
 write.csv(climatebc_locs, "locations/climatebc_locs.csv", row.names = FALSE, eol = "\r\n")
+
+
+# parent ####
+
+pardat <- read.csv("locations/ParentTreeExtractReport_ParentTrees_2014_05_15_13_38_17.csv")
+
+# parent locations and elevations
+parent_locs <- pardat %>%
+    select(Parent.Tree.Number, contains("Latitude"), contains("Longitude"), Elevation, Seed.Plan.Zone.Code) %>%
+    mutate(lat = Latitude.Degrees + Latitude.Minutes/60 + Latitude.Seconds/60^2,
+           lon = -(Longitude.Degrees + Longitude.Minutes/60 + Longitude.Seconds/60^2)) %>%
+    select(Clone = Parent.Tree.Number, SPU = Seed.Plan.Zone.Code, lat, lon, el = Elevation)
+
+write.csv(parent_locs, "locations/parent_locs.csv", row.names = FALSE)
+
+# parent locations and elevations formatted for climatebc
+climatebc_parent_locs <- parent_locs %>%
+    rename(id1 = Clone, id2 = SPU)
+write.csv(climatebc_parent_locs, "locations/climatebc_parent_locs.csv", row.names = FALSE, eol = "\r\n")
